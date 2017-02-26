fcfa3679ae16012b233f37d7b1e39ec055e50a67
SamuelOliveira <samuca95@hotmail.com>
2017-02-26 01:34:44 -0300

    Innitial Commit

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-/*
- * This file is generated and updated by Sencha Cmd. You can edit this file as
- * needed for your application, but these edits will have to be merged by
- * Sencha Cmd when upgrading.
- */
+//@charset UTF-8
+Ext.Loader.setConfig({
+    enabled: true,
+    paths: {
+        'Smart.data': 'library/data',
+        'Smart.data.proxy': 'library/data/proxy'
+    }
+});
+
 Ext.application({
     name: 'Cryptic',
 
@@ -12,15 +16,6 @@ Ext.application({
         'Cryptic.view.main.Main'
     ],
 
-    // The name of the initial view to create. With the classic toolkit this class
-    // will gain a "viewport" plugin if it does not extend Ext.Viewport. With the
-    // modern toolkit, the main view will be added to the Viewport.
-    //
     mainView: 'Cryptic.view.main.Main'
 	
-    //-------------------------------------------------------------------------
-    // Most customizations should be made to Cryptic.Application. If you need to
-    // customize this file, doing so below this section reduces the likelihood
-    // of merge conflicts when upgrading to new versions of Sencha Cmd.
-    //-------------------------------------------------------------------------
 });
